2234277317268791facba16a8f29ed4e45c2dae1
Deon Choi <deon.choi@gmail.com>
2020-01-05 22:16:13 -0800

    heroku deployment

--- server.js
+++ server.js
@@ -29,12 +29,10 @@ const collectionRouter = require('./routes/collection');
 app.use('/user', userRouter);
 app.use('/collection', collectionRouter);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
-};
     
 
 
