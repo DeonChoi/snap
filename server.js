e54f830008876eaf26cdca94fbe77aa8a726e7f1
Deon Choi <deon.choi@gmail.com>
2020-01-05 21:43:16 -0800

    build routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 
@@ -24,8 +25,18 @@ connection.once('open', () => {
 const userRouter = require('./routes/user');
 const collectionRouter = require('./routes/collection');
 
+
 app.use('/user', userRouter);
 app.use('/collection', collectionRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
+    
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
